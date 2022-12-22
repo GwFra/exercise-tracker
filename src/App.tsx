a1c6dcf1963f9eea81d1bd172c6fa757e6c9d78e
George Franklin <george.franklin66@gmail.com>
2022-12-22 19:23:18 +0000

    Enforcing linting rules

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import "./App.css";
 import { Avatar, Stack } from "@mui/material";
 
 function App () {
-	return (
-		<div className="App">
-			<span>Welcome John</span>
-			<Stack direction={"row"} spacing={2}>
-				<Avatar>J</Avatar>
-			</Stack>
-		</div>
-	);
+  return (
+    <div className="App">
+      <span>Welcome John</span>
+      <Stack direction={"row"} spacing={2}>
+        <Avatar>J</Avatar>
+      </Stack>
+    </div>
+  );
 }
 
 export default App;
