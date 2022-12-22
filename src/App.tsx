aac6a0e6a33f4b6c4a6e79c102b203e805e98b6e
George Franklin <george.franklin66@gmail.com>
2022-12-22 17:39:08 +0000

    Sorted out linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react'
-import './App.css';
-import { Avatar, Stack } from '@mui/material';
+import * as React from "react";
+import "./App.css";
+import { Avatar, Stack } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <span>Welcome John</span>
-      <Stack direction={"row"} spacing={2}>
-        <Avatar>J</Avatar>
-      </Stack>
-    </div>
-  );
+function App () {
+	return (
+		<div className="App">
+			<span>Welcome John</span>
+			<Stack direction={"row"} spacing={2}>
+				<Avatar>J</Avatar>
+			</Stack>
+		</div>
+	);
 }
 
 export default App;
