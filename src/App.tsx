d39c92c53b358a98efa8c0122fa7767b318be59c
George Franklin <george.franklin66@gmail.com>
2023-01-07 00:17:55 +0000

    Added basic example of theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import * as React from "react";
+import React, { useContext, useState } from "react";
 import "./App.css";
 import { Avatar, Stack } from "@mui/material";
+import ThemeContext, { ThemeProvider, themes } from "./contexts/theme";
+
+function ThemeButton (props: {toggleTheme: () => void}) {
+  const theme = useContext(ThemeContext);
+  console.log(theme);
+  return (
+    <div onClick={props.toggleTheme} style={{backgroundColor: theme.background}}>
+      Click me to change theme
+    </div>
+  );
+}
 
 function App () {
+  const [theme, setTheme] = useState(themes.light);
+  const toggleTheme = () => {
+    setTheme(theme === themes.light ? themes.dark : themes.light);
+  };
   return (
     <div className="App">
       <span>Welcome John</span>
       <Stack direction={"row"} spacing={2}>
         <Avatar>J</Avatar>
       </Stack>
+      <ThemeProvider value={theme} >
+        <ThemeButton toggleTheme={toggleTheme} />
+      </ThemeProvider>
     </div>
   );
 }
